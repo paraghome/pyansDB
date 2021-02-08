adc2c9122bee77cdbdddb2551b1ea064d2fb55a2
Parag <student@bchd.98d0af42-5766-4467-a679-769ac6613b05>
2021-02-08 19:43:16 +0000

    lists and looping

---
+++ mix-list/mix_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+my_list = ['192.168.0.5', 5060, 'UP']
+print('IP = ' + my_list[0])
+print('port = ' + str(my_list[1]))
+print('state = ' + my_list[2])
+
+new_list=[5060, "80", 55, "10.0.0.1", "10.0.0.2", "ssh"]
+
+print("The IP addresses are " +  new_list[3] + " and " + new_list[4])
+
+
